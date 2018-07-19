2f781ac62de6dc8bcd53d7bd4b814bffc8859c44
Maxyee <eyaminkhan00@gmail.com>
2018-07-19 18:55:03 +0600

    Finding the mathmaticians pageview by python auto scrapping

--- venv/scrapping/mathematicians.py
+++ venv/scrapping/mathematicians.py
@@ -47,9 +47,76 @@ def get_names():
 
     raise Exception('Error retrieving contents at {}'.format(url))
 
-    
-raw_html = simple_get('http://www.fabpedigree.com/james/mathmen.htm')
-html = BeautifulSoup(raw_html, 'html.parser')
+# print(get_names())
 
-for i, li in enumerate(html.select('li')):
-    print(i,li.text)
+
+def get_hits_on_name(name):
+
+    url_root = 'https://xtools.wmflabs.org/articleinfo/en.wikipedia.org/{}'
+    response = simple_get(url_root.format(name))
+
+    if response is not None:
+        html = BeautifulSoup(response, 'html.parser')
+        hit_link = [a for a in html.select('a')
+                    if a['href'].find('latest-60') > -1 ]
+
+        if len(hit_link) > 0:
+            link_text = hit_link[0].text.replace(',','')
+
+        try:
+            return int(link_text)
+        
+        except:
+            log_error("couldn't parse {} as an `int`".format(link_text))
+
+    log_error('No Pageviews found for {}'.format(name))
+    return None
+
+# print(get_hits_on_name('Apollonius  of Perga'))
+
+
+if __name__ == '__main__':
+
+    print('Getting the list of names......')
+    names = get_names()
+    print('....done.\n')
+
+    results = []
+
+    print('Getting stats for each name.....')
+
+    for name in names:
+        try:
+            hits = get_hits_on_name(name)
+            if hits is None:
+                hits = -1
+            
+            results.append((hits, name))
+        
+        except:
+            results.append((-1, name))
+            log_error('error encountered while processing '
+                      '{}, skipping'.format(name))
+
+    print('...done.\n')
+
+    results.sort()
+    results.reverse()
+
+    if len(results) > 5:
+        top_marks = results[:5]
+    else:
+        top_marks = results
+
+    print('\nThe most popular mathematicians are:\n')
+    for (mark, mathematician) in top_marks:
+        print('{} with {} pageviews'.format(mathematician, mark))
+
+    no_results = len([res for res in results if res[0] == -1])
+    print('\nBut we did not find results for '
+          '{} mathematicians on the list'.format(no_results))
+# raw_html = simple_get('http://www.fabpedigree.com/james/mathmen.htm')
+# html = BeautifulSoup(raw_html, 'html.parser')
+
+# for i, li in enumerate(html.select('li')):
+#     print(i,li.text)
